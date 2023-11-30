109c419d4a68789e03d1eaa5503990fe68ff0bac
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2023-11-30 16:55:41 +0100

    ADDED working facetwp logic

--- js/list.js
+++ js/list.js
@@ -1,30 +1,40 @@
-( function( document, Locations, L ) {
+function render_rpi_map() {
 
-
+    window.Locations = JSON.parse(document.getElementById('locations').innerHTML);
+    console.log(Locations);
 
     // Initialize the map.
     var map = L.map('map').setView([51.319, 9.4949], 7);
 
+    window.map = map;
     // Set tile layer for Open Street Map.
-    var tileLayer = L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    } );
+    });
 
-    map.addLayer( tileLayer );
+    map.addLayer(tileLayer);
 
+
+    markers = render_rpi_map_marker()
+
+    map.addLayer(markers);
+
+}
+
+function render_rpi_map_marker() {
     let iconOpt = {
         iconSize: [36, 36],
         iconAnchor: [36, 36],
         popupAnchor: [-18, -36],
 
     }
-
     var markers = L.markerClusterGroup();
 
-    // Show marker for each location.
-    Locations.forEach( function( location ) {
+    console.log(Locations);
 
-        console.log(location);
+    // Show marker for each location.
+    Locations.forEach(function (location) {
+
         iconOpt.iconUrl = location.iconUrl;
         // Marker options.
 
@@ -32,18 +42,19 @@
             title: location.title,
         };
 
-        if(iconOpt.iconUrl.length>0){
-            options.icon= L.icon(iconOpt);
+        if (iconOpt.iconUrl.length > 0) {
+            options.icon = L.icon(iconOpt);
 
         }
 
-        var center = L.latLng( location.lat, location.lng )
-        var marker = L.marker( center, options );
+        var center = L.latLng(location.lat, location.lng)
+        var marker = L.marker(center, options);
 
         // Show name of the restaurant when click on the icon.
-        marker.bindPopup( '<b>' + location.title + '</b><br>' + location.address + '<hr>' + location.text).openPopup();
+        marker.bindPopup('<b>' + location.title + '</b><br>' + location.address + '<hr>' + location.text).openPopup();
         markers.addLayer(marker);
-    } );
-    map.addLayer(markers);
 
-} )( document, Locations, L );
+    });
+
+    return markers;
+}
--- rpi-osm.php
+++ rpi-osm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       rpi Open Street Maps
  * Plugin URI:        https://github.com/rpi-virtuell/rpi-osm
@@ -11,46 +12,72 @@
  * GitHub Plugin URI: https://github.com/rpi-virtuell/rpi-osm
  * GitHub Branch:     master
  */
-class RPI_OSM{
+class RPI_OSM
+{
     public function __construct()
     {
-        add_action( 'wp_enqueue_scripts', function() {
+        add_action('wp_enqueue_scripts', function () {
 
-                wp_enqueue_style( 'leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', [], '1.9.4' );
+            wp_enqueue_style('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', [], '1.9.4');
 
-                wp_enqueue_style( 'leaflet_cluster_default', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css', [], '1.4.1' );
-                wp_enqueue_style( 'leaflet_cluster', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css', [], '1.4.1' );
+            wp_enqueue_style('leaflet_cluster_default', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css', [], '1.4.1');
+            wp_enqueue_style('leaflet_cluster', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css', [], '1.4.1');
 
-                wp_enqueue_script( 'leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', [], '1.9.4', true );
-                wp_enqueue_script( 'leaflet_cluster', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', [], '1.4.1', true );
+            wp_enqueue_script('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', [], '1.9.4', true);
+            wp_enqueue_script('leaflet_cluster', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', [], '1.4.1', true);
 
-                wp_enqueue_script( 'list', plugins_url( 'js/list.js' , __FILE__), ['jquery', 'leaflet'], '1.0', true );
+            wp_enqueue_script('list', plugins_url('js/list.js', __FILE__), ['jquery', 'leaflet'], '1.0', true);
 
-                $locations = [];
-                $query = new WP_Query( [
-                    'post_type' => 'oum-location',
-                ] );
-                /** Beispiel Post Meta einer Open User Map Location:
-                   $location =  get_post_meta($post->ID , '_oum_location_key', true );
-                   [
-                     'address' => 'Das CI',
-                     'lat' => 51.96604085,
-                     'lng' => 7.59554464469681,
-                     'text' => 'Inhalt',
-                     'author_name' => '',
-                     'author_email' => '',
-                   ] */
-                foreach ( $query->posts as $post ) {
+//                $locations = [];
+//                $query = new WP_Query( [
+//                    'post_type' => 'location',
+//                    'facetwp'=> true,
+//                ] );
+//                /** Beispiel Post Meta einer Open User Map Location:
+//                   $location =  get_post_meta($post->ID , '_oum_location_key', true );
+//                   [
+//                     'address' => 'Das CI',
+//                     'lat' => 51.96604085,
+//                     'lng' => 7.59554464469681,
+//                     'text' => 'Inhalt',
+//                     'author_name' => '',
+//                     'author_email' => '',
+//                   ] */
+//                foreach ( $query->posts as $post ) {
+//
+//                    $location =  get_post_meta($post->ID , 'osm_location', true );
+//                    $location['title']   = $post->post_title;
+//                    $location['iconUrl']= get_post_meta($post->ID , '_oum_location_image', true );
+//
+//                    $locations[]         = $location;
+//                }
+//                wp_localize_script( 'list', 'Locations', $locations );
 
-                    $location =  get_post_meta($post->ID , '_oum_location_key', true );
-                    $location['title']   = $post->post_title;
-                    $location['iconUrl']= get_post_meta($post->ID , '_oum_location_image', true );
 
-                    $locations[]         = $location;
-                }
-                wp_localize_script( 'list', 'Locations', $locations );
+        });
 
-        } );
+
+        add_action('wp_footer', function () {
+            ?>
+            <script>
+                document.addEventListener('facetwp-loaded', function (e, o) {
+                    setTimeout(function () {
+
+                        if (map._loaded) {
+                            map.remove();
+
+                        }
+                        render_rpi_map();
+                        console.log('trigger');
+                    }, 0);
+
+
+                });
+            </script>
+            <?php
+        }, 100);
+
     }
 }
+
 new RPI_OSM();
